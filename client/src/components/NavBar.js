4fbdfbad367ac1d9981f9ffbe9e6d806542b9ad7
rakibtg <rakibtg@gmail.com>
2019-08-31 21:00:52 +0600

    navbar design

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import { Pane, Button, Text, Heading, Icon } from 'evergreen-ui'
-import DockerContainer from './DockerContainer'
-import DockerImage from './DockerImage'
-import Stats from './Stats'
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { Pane, Icon, Text, Badge, SegmentedControl } from 'evergreen-ui'
 
+class NavBar extends React.PureComponent {
 
-const NavBar = () => {
-    return (
-    <Pane display="flex" padding={16} background="#14B5D0" borderRadius={3} paddingLeft={20} paddingRight={20}>
-      <Pane flex={1} alignItems="center" display="flex">
-      <Icon icon="dashboard" color="#F9F9FB" marginRight={16} />
-      </Pane>
-      <Pane color="white">
-        <Link to='/' style={{ textDecoration: 'none', textTransform: 'uppercase', color: 'white' }}>Containers</Link>&nbsp;|&nbsp; 
-        <Link to='/images' style={{ textDecoration: 'none', textTransform: 'uppercase', color: 'white' }}>Images</Link> &nbsp;| &nbsp;
-      < Link to='/stats' style={{ textDecoration: 'none', textTransform: 'uppercase', color: 'white' }}>Stats</Link>
-      </Pane>
+  navButton (name, icon) {
+    return <Text display='flex' alignItems='center'>
+      <Icon size={14} color="muted" icon={ icon } marginRight={5}/> 
+        { name }
+    </Text>
+  }
+
+  render () {
+    return <Pane display="flex" justifyContent="center" padding={26} background="#f9f9fc">
+      <SegmentedControl
+        width={750}
+        height={50}
+        options={[
+          { label: this.navButton( 'Container', 'layers' ), value: 'container' },
+          { label: this.navButton( 'Image', 'projects' ), value: 'image' },
+          { label: this.navButton( 'Usage Stat', 'chart' ), value: 'stat' },
+          { label: this.navButton( 'Clean-up', 'shield' ), value: 'cleanup' }
+        ]}
+        onChange={value => console.log(value)}
+      />
     </Pane>
-    );
-};
+  }
+}
 
-export default NavBar;
+// const NavBar = () => {
+//     return (
+//     <Pane display="flex" padding={16} background="#f9f9fc">
+//       <Pane flex={1} alignItems="center" display="flex">
+//         <Icon icon="dashboard" color="dark" marginRight={16} />
+//       </Pane>
+//       <Pane color="black">
+//         <Link to='/' style={{ textDecoration: 'none', textTransform: 'uppercase', color: 'black' }}>Containers</Link>&nbsp;|&nbsp; 
+//         <Link to='/images' style={{ textDecoration: 'none', textTransform: 'uppercase', color: 'black' }}>Images</Link> &nbsp;| &nbsp;
+//       < Link to='/stats' style={{ textDecoration: 'none', textTransform: 'uppercase', color: 'black' }}>Stats</Link>
+//       </Pane>
+//     </Pane>
+//     )
+// }
+
+export default NavBar
