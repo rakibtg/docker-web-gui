df69c1fe47c45667b3d08a700f94012fb5eee438
rakibtg <rakibtg@gmail.com>
2019-08-31 21:00:17 +0600

    container list design

--- client/src/components/ContainerListItem.js
+++ client/src/components/ContainerListItem.js
@@ -1,21 +1,47 @@
 import React from 'react'
 import { Pane, Button, Heading, Badge, Switch } from 'evergreen-ui'
+import './ElementCard.css'
 
-const ContainerListItem = () => {
-    return (
-        <Pane border="default" padding={10} marginBottom={10}>
-           <Pane display="flex">
-               <Heading size={500}>Container name</Heading>
-               <Badge color="green" marginLeft={10} marginTop={3}>Running</Badge>
-           </Pane>
-           <Pane display="flex" marginTop={10}>
-               <Switch marginRight={12} marginTop={2}/>
-               <Button marginRight={12} height={20} iconBefore="edit" appearance="primary" intent='warning'>Edit</Button>
-               <Button marginRight={12} height={20} iconBefore="delete" appearance="primary" intent='danger'>Delete</Button>
-               <Button marginRight={12} height={20} iconBefore="application" appearance="primary" intent='none'>Log</Button>
-           </Pane>
+class ContainerList extends React.PureComponent {
+
+    state = {
+        active: false,
+    }
+
+    handleHover (active) {
+        this.setState({ active })
+    }
+
+    render () {
+        const { active } = this.state
+        return <Pane 
+                display="flex" 
+                flexDirection="column" 
+                flexGrow={1}
+                padding={12}
+                borderRadius={6}
+                border="default"
+                className="element-card"
+                onMouseEnter={() => this.handleHover(true)}
+                onMouseLeave={() => this.handleHover(false)}>
+            <Pane display="flex">
+                <Pane display="flex" justifyContent="center" alignItems="center">
+                    <Switch marginRight={10} height={18} marginTop={2}/> 
+                    <Heading size={500}>Container name</Heading>
+                </Pane>
+                <Badge backgroundColor="#e7e9ef" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>3da3ad7b90e3</Badge>
+                <Badge backgroundColor="#deebf7" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>cpu 0.5%</Badge>
+                <Badge backgroundColor="#ebe7f8" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>ram 423.3 mb</Badge>
+            </Pane>
+            { active && 
+                <Pane display="flex" marginTop={12} marginLeft={46}>
+                    <Button marginRight={5} height={22} iconBefore="refresh">Restart</Button>
+                    <Button marginRight={5} height={22} iconBefore="application">Log</Button>
+                    <Button marginRight={5} height={22} iconBefore="trash">Delete</Button>
+                </Pane>
+            }
         </Pane>
-    );
-};
+    }
+}
 
-export default ContainerListItem
+export default ContainerList
