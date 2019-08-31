c267af335b3f28ae342b89260d085f0170d8a456
rakibtg <rakibtg@gmail.com>
2019-08-31 12:30:06 +0600

    added container endpoints

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,15 @@ const port = 3230
 
 const { DefaultController } = require('./controllers/DefaultController')
 const { GenericCommandController } = require('./controllers/GenericCommandController')
+const ContainerController = require('./controllers/ContainerController')
 
 app.get('/', DefaultController)
 app.get('/api/generic', GenericCommandController)
+
+app.get('/api/controller/fetch', ContainerController.fetch)
+app.get('/api/controller/fetchById', ContainerController.fetchById)
+app.get('/api/controller/command', ContainerController.command)
+app.get('/api/controller/logs', ContainerController.logs)
+app.get('/api/controller/stats', ContainerController.stats)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
