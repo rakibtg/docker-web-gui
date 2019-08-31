1721e11a14aed6b9eef06323d29eda0d2a876ebb
rakibtg <rakibtg@gmail.com>
2019-08-31 21:00:31 +0600

    added sec navbar

--- client/src/components/DockerContainer.js
+++ client/src/components/DockerContainer.js
@@ -1,19 +1,21 @@
 import React from 'react'
+import { Pane, Heading } from 'evergreen-ui'
+
 import ContainerListItem from './ContainerListItem'
-import { Pane, Heading, SearchInput } from 'evergreen-ui'
+import SecondaryNavBar from './SecondaryNavBar'
 
 const DockerContainer = () => {
     return (
         <Pane padding={20}>
-            <Heading size={600}>Containers</Heading>
-            <SearchInput placeholder="search container" width="100%" marginBottom={10}/>
-            {['1', '2', '3', '4'].map(i => {
-                return (
-                <ContainerListItem/>
-                )
-            })}
+            <Heading />
+            <SecondaryNavBar />
+            <Pane display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+                {['1', '2', '3', '4'].map(i => {
+                    return <ContainerListItem key={i}/>
+                })}
+            </Pane>
         </Pane>
-    );
-};
+    )
+}
 
-export default DockerContainer;
+export default DockerContainer
