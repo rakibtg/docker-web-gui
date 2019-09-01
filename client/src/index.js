0cadebb4530ee2c0a4990d44932882001fb63aa6
rakibtg <rakibtg@gmail.com>
2019-09-02 00:12:27 +0600

    added reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
 import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
-import reducers from './reducers'
+import reducers from './store/reducers'
 import Routes from './routes'
 
 
