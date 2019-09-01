183b7c12d96adea40933f49de216f8078f787f37
rakibtg <rakibtg@gmail.com>
2019-09-02 00:15:38 +0600

    container stat badge

--- client/src/components/ContainerListItem.js
+++ client/src/components/ContainerListItem.js
@@ -29,9 +29,10 @@ class ContainerList extends React.PureComponent {
                     <Switch marginRight={10} height={18} marginTop={2}/> 
                     <Heading size={500}>Container name</Heading>
                 </Pane>
-                <Badge backgroundColor="#e7e9ef" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>3da3ad7b90e3</Badge>
-                <Badge backgroundColor="#deebf7" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>cpu 0.5%</Badge>
-                <Badge backgroundColor="#ebe7f8" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>ram 423.3 mb</Badge>
+                <Badge backgroundColor="#e7e9ef" width="120px" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>3da3ad7b90e3</Badge>
+                <Badge backgroundColor="#d4eee3" width="80px" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>nov 6</Badge>
+                <Badge backgroundColor="#deebf7" width="120px" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>cpu 0.5%</Badge>
+                <Badge backgroundColor="#ebe7f8" width="120px" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>ram 423.3 mb</Badge>
             </Pane>
             { active && 
                 <Pane display="flex" marginTop={12} marginLeft={46}>
