2ef1574fb2dd094327952d3b035c3158b8e4a814
rakibtg <rakibtg@gmail.com>
2019-09-02 23:15:42 +0600

    added getContainers and genericContainer actions

--- client/src/store/actions/container.action.js
+++ client/src/store/actions/container.action.js
@@ -1,4 +1,22 @@
-export const getContainers = payload => ({
-  type: 'GET_CONTAINERS',
+import { request } from '../../utilities/request'
+
+export const genericContainer = payload => ({
+  type: 'GENERIC_CONTAINER',
   payload
 })
+
+export const getContainers = token => {
+  return dispatch => {
+    request('post', 'container', {})
+      .then(response => {
+        console.log(response)
+        // dispatch(genericContainer({
+        //   homeScreenLoading: false
+        // }))
+      }).catch(error => {
+        // dispatch(genericContainer({
+        //   homeScreenLoading: false
+        // }))
+      })
+  }
+}
