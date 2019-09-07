8555fee14abe92d6f0b57b4bd9d821b4ded96fed
rakibtg <rakibtg@gmail.com>
2019-09-07 18:41:09 +0600

    consume activeIndex from store

--- client/src/components/container/card.js
+++ client/src/components/container/card.js
@@ -2,19 +2,15 @@ import React from 'react'
 import { Pane, Button, Heading, Badge, Switch } from 'evergreen-ui'
 import './style/card.css'
 
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { genericContainer } from '../../store/actions/container.action'
+
 class ContainerCard extends React.PureComponent {
 
-    state = {
-        active: false,
-    }
-
-    handleHover (active) {
-        this.setState({ active })
-    }
-
     render () {
-        const { active } = this.state
-        const { container } = this.props
+      const { container, activeIndex, genericContainer, index } = this.props
+      const active = activeIndex == index
         return <Pane 
                 display="flex" 
                 flexDirection="column" 
@@ -22,18 +18,19 @@ class ContainerCard extends React.PureComponent {
                 padding={12}
                 borderRadius={6}
                 border="default"
-                className="element-card"
-                onMouseEnter={() => this.handleHover(true)}
-                onMouseLeave={() => this.handleHover(false)}>
+                className={active ? "element-card card-active" : "element-card"}
+                onMouseEnter={() => genericContainer({
+                  activeIndex: index
+                })}>
             <Pane display="flex">
                 <Pane display="flex" justifyContent="center" alignItems="center">
                     <Switch checked={container.State.Running} marginRight={10} height={18} marginTop={2}/> 
                     <Heading size={400}>{container.Name}</Heading>
                 </Pane>
-                <Badge backgroundColor="#e7e9ef" width="120px" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>{container.shortId}</Badge>
-                <Badge backgroundColor="#d4eee3" width="80px" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>nov 6</Badge>
-                <Badge backgroundColor="#deebf7" width="120px" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>cpu 0.5%</Badge>
-                <Badge backgroundColor="#ebe7f8" width="120px" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>ram 423.3 mb</Badge>
+                <Badge backgroundColor="#e7e9ef" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>{container.shortId}</Badge>
+                <Badge backgroundColor="#d4eee3" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>nov 6</Badge>
+                <Badge backgroundColor="#deebf7" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>cpu 0.5%</Badge>
+                <Badge backgroundColor="#ebe7f8" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>ram 423.3 mb</Badge>
             </Pane>
             { active && 
                 <Pane display="flex" marginTop={12} marginLeft={46}>
@@ -46,4 +43,17 @@ class ContainerCard extends React.PureComponent {
     }
 }
 
-export default ContainerCard
+const mapStateToProps = state => {
+  return {
+    activeIndex: state.container.activeIndex
+  }
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators(
+  {
+    genericContainer
+  },
+  dispatch
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)( ContainerCard )
