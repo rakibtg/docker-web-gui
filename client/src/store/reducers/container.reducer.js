55c506ba761fe572b9f01fbe9417c154ad841a35
rakibtg <rakibtg@gmail.com>
2019-09-07 21:28:39 +0600

    update container specific info

--- client/src/store/reducers/container.reducer.js
+++ client/src/store/reducers/container.reducer.js
@@ -8,6 +8,23 @@ export default (state = null, action) => {
         ...action.payload
       }
 
+    case 'UPDATE_CONTAINER':
+      return {
+        ...state,
+        ...{
+          containers: state.containers.map(c => {
+            if(c.shortId == action.payload.containerId) {
+              return {
+                ...c,
+                ...action.payload.data
+              }
+            } else {
+              return c
+            }
+          })
+        }
+      }
+
     default:
       return state
 
