f8214f1698bb37abf2901c77d4c9b3e5eda04fe5
rakibtg <rakibtg@gmail.com>
2019-09-07 22:23:49 +0600

    removed old components

--- client/src/components/DockerImage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DockerImage = () => {
-    return (
-        <div>Image</div>
-    );
-};
-
-export default DockerImage;
--- client/src/components/Stats.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Stats = () => {
-    return (
-        <div>Stats</div>
-    );
-};
-
-export default Stats;
