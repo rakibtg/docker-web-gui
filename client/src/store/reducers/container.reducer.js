ef5655e8cf2437d707819115d8beeee5e39b1353
castor_a <akash.jucse@gmail.com>
2019-09-10 23:35:03 +0600

    added delete reducer

--- client/src/store/reducers/container.reducer.js
+++ client/src/store/reducers/container.reducer.js
@@ -24,6 +24,16 @@ export default (state = null, action) => {
           })
         }
       }
+    
+    case 'DELETE_CONTAINER':
+      return {
+        ...state,
+        ...{
+          containers: state.containers.filter(c => {
+            return c.shortId == action.payload.containerId
+          })
+        }
+      }
 
     default:
       return state
