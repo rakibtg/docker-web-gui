a1389d77702483fe764b624ecd25c37316a009d1
rakibtg <rakibtg@gmail.com>
2019-09-12 21:59:09 +0600

    added image fetch method

--- backend/controllers/ImageController.js
+++ backend/controllers/ImageController.js
@@ -1,2 +1,12 @@
 const { Terminal } = require('../utilities/terminal')
 
+exports.fetch = async (req, res) => {
+  const images = await Terminal(
+    `docker image ls -a --format '{"ID": "{{.ID}}", "CreatedSince": "{{.CreatedSince}}", "Size": "{{.Size}}", "VirtualSize": "{{.VirtualSize}}", "Repository": "{{.Repository}}"}'`
+  )
+  const imagesArray = images
+    .split("\n")
+    .filter(image => image !== '')
+    .map(image => JSON.parse(image))
+  res.json(imagesArray)
+}
