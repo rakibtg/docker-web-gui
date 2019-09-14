87b73dc6e757df6258830a2a12a2b143a87e2368
rakibtg <rakibtg@gmail.com>
2019-09-14 21:44:55 +0600

    boot db method done

---
+++ backend/utilities/db.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const resolve = require('path').resolve
+const knexLibrary = require('knex')
+const write = require('write')
+const fileExists = require('file-exists')
+
+exports.dbSource = resolve(__dirname+'/../data.db')
+
+exports.knex = knexLibrary({
+  client: 'sqlite3',
+  connection: {
+    filename: this.dbSource
+  }
+})
+
+exports.boot = () => {
+  fileExists(this.dbSource)
+    .then( async exists => {
+      if( !exists ) {
+        // Create the file.
+        await write(this.dbSource, '')
+        // Create tables.
+        await this.knex.schema.createTable('groups', function (table) {
+          table.increments()
+          table.string('name')
+          table.text('containers_id')
+          table.timestamps()
+        })
+      }
+    })
+}
+
+exports.newGroup = () => {
+
+}
+
+exports.deleteGroup = () => {
+
+}
+
+exports.getGroups = () => {
+
+}
