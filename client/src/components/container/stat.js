48a268a5ca588e86e4682d3a5b9ca866a986353d
rakibtg <rakibtg@gmail.com>
2019-09-14 12:23:07 +0600

    added live system usage stat

--- client/src/components/container/stat.js
+++ client/src/components/container/stat.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import { Badge } from 'evergreen-ui'
+import { connect } from 'react-redux'
+
 
 class ContainerStat extends React.PureComponent {
+  
+  getMemoryUsage(memory) {
+    [memory] = memory.split('/')
+    let memoryFormated = memory.replace(/[a-zA-Z]/g, '').trim()
+    return Number(memoryFormated).toFixed(1) + 'mb'
+  }
+
+  renderBadges () {
+    const { stats, containerID } = this.props
+    const data = stats
+      .find(n => n.id === containerID)
+    return data 
+      ? <>
+        <Badge backgroundColor="#deebf7" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>
+          cpu {data.cpu_percentage}
+        </Badge>
+        <Badge backgroundColor="#ebe7f8" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>
+          ram {this.getMemoryUsage(data.memory_usage)}
+        </Badge>
+        <Badge backgroundColor="#ebe7f8" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>
+          net {this.getMemoryUsage(data.network_io)}
+        </Badge>
+      </>
+      : null
+  }
+
   render () {
-    return <>
-      <Badge backgroundColor="#deebf7" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>cpu 0.5%</Badge>
-      <Badge backgroundColor="#ebe7f8" fontWeight="bold" borderRadius={16} paddingLeft={10} fontSize={11} paddingRight={10} marginLeft={10} marginTop={3}>ram 423.3 mb</Badge>
-    </>
+    return this.renderBadges()
   }
 }
 
-export default ContainerStat
+const mapStateToProps = state => {
+  return {
+    stats: state.stats.containerStats
+  }
+}
+
+export default connect(mapStateToProps, null)( ContainerStat )
