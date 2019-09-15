f4f3654d4240a7d9fa14b07c5bd08ad8670854e5
rakibtg <rakibtg@gmail.com>
2019-09-15 21:29:42 +0600

    render components based on store group data

--- client/src/components/SecondaryNavBar.js
+++ client/src/components/SecondaryNavBar.js
@@ -11,49 +11,76 @@ import NewGroupForm from './NewGroupForm'
 
 class SecondaryNavBar extends React.PureComponent {
 
+  containerFilters () {
+    const { loading, segment, getContainers } = this.props
+    return <SegmentedControl
+      width={400}
+      height={26}
+      options={[
+        { label: loading === 'all' ? <Spinner size={16} /> : 'All', value: 'all' },
+        { label: loading === 'active' ? <Spinner size={16} /> : 'Active', value: 'active' },
+        { label: loading === 'stopped' ? <Spinner size={16} /> : 'Stopped', value: 'stopped' }
+      ]}
+      value={segment}
+      onChange={value => {
+        getContainers(value)
+      }}
+    />
+  }
+
+  newGroupButton () {
+    const { showNewGroupForm, genericGroups } = this.props
+    return <Button 
+      marginRight={12} 
+      iconBefore='add'
+      paddingLeft={35}
+      paddingRight={30}
+      height={26}
+      onClick={() => {
+        genericGroups({
+          showNewGroupForm: !showNewGroupForm
+        })
+      }}>Create New Group</Button>
+  }
+
+  groupsToggler () {
+    const { genericGroups, showGroupsPage } = this.props
+    return <Button 
+      marginRight={12} 
+      iconBefore={showGroupsPage ? 'chevron-left' : 'tag'}
+      paddingLeft={35}
+      paddingRight={30}
+      height={26}
+      onClick={() => {
+        genericGroups({ 
+          showGroupsPage: !showGroupsPage,
+          showNewGroupForm: false,
+        })
+      }}>
+        {showGroupsPage ? 'Back' : 'Groups'}
+    </Button>
+  }
+
+  renderBody () {
+    const { showNewGroupForm, showGroupsPage } = this.props
+    if(showNewGroupForm) {
+      return <NewGroupForm />
+    } else if (showGroupsPage) {
+      return this.newGroupButton()
+    } else {
+      return this.containerFilters()
+    }
+  }
+
   render() {
-    const { 
-      segment, 
-      loading, 
-      getContainers, 
-      genericGroups, 
-      showGroupsPage,
-    } = this.props
     return <Pane 
       backgroundColor="#f1f1f1" 
       display="flex" 
       justifyContent="center"
       padding={10}>
-      <Button 
-        marginRight={12} 
-        iconBefore={showGroupsPage ? 'chevron-left' : 'tag'}
-        paddingLeft={35}
-        paddingRight={30}
-        height={26}
-        onClick={() => {
-          genericGroups({ showGroupsPage: !showGroupsPage })
-        }}>
-          {showGroupsPage ? 'Back' : 'Groups'}
-      </Button>
-      {
-        showGroupsPage 
-          ? <NewGroupForm />
-          : <SegmentedControl
-              width={400}
-              height={26}
-              options={[
-                { label: loading === 'all' ? <Spinner size={16} /> : 'All', value: 'all' },
-                { label: loading === 'active' ? <Spinner size={16} /> : 'Active', value: 'active' },
-                { label: loading === 'stopped' ? <Spinner size={16} /> : 'Stopped', value: 'stopped' }
-              ]}
-              value={segment}
-              onChange={value => {
-                getContainers(value)
-              }}
-            />
-
-      }
-    </Pane>
+        {this.groupsToggler()}
+        {this.renderBody()}
+      </Pane>
   }
 }
 
@@ -62,6 +89,7 @@ const mapStateToProps = state => {
     segment: state.container.segment,
     loading: state.container.loading,
     showGroupsPage: state.groups.showGroupsPage,
+    showNewGroupForm: state.groups.showNewGroupForm,
   }
 }
 
