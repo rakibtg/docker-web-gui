0b4936d26c93f51169f0a7aff686041ab91a41fa
rakibtg <rakibtg@gmail.com>
2019-09-15 19:11:01 +0600

    added groups in the store

--- client/src/store/schema/index.js
+++ client/src/store/schema/index.js
@@ -1,7 +1,9 @@
-import container from './container.schema'
 import stats from './stats.schema'
+import groups from './groups.schema'
+import container from './container.schema'
 
 export default {
   stats,
+  groups,
   container,
 }
