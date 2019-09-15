74464e084f50ef351aa9bdadd99d9c9cb104fcee
rakibtg <rakibtg@gmail.com>
2019-09-15 20:16:06 +0600

    Added new group form

---
+++ client/src/components/NewGroupForm.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Pane, Button, TextInput } from 'evergreen-ui'
+
+class NewGroupForm extends React.PureComponent {
+  render () {
+    return <Pane 
+      display='flex'
+      justifyContent='center'
+      alignItems='center'>
+        <TextInput
+          name="text-input-name"
+          placeholder="Write the group name here"
+          height={26}
+          display='flex'
+          flexGrow={1}
+        />
+        <Button
+          height={26}
+          appearance="primary"
+          marginLeft={12}
+          intent="success"
+        >
+          Create Group
+        </Button>
+    </Pane>
+  }
+}
+
+export default NewGroupForm
