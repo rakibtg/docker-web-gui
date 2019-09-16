dc755ad247b660913abe403f02c21ff136d49c8a
rakibtg <rakibtg@gmail.com>
2019-09-16 23:37:12 +0600

    toggle create group button based on container selection

--- client/src/components/NewGroupForm.js
+++ client/src/components/NewGroupForm.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Pane, Button, TextInput } from 'evergreen-ui'
 
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
 class NewGroupForm extends React.PureComponent {
   render () {
+    const { selectedItems } = this.props
     return <Pane 
       display='flex'
       justifyContent='center'
@@ -19,6 +23,7 @@ class NewGroupForm extends React.PureComponent {
           appearance="primary"
           marginLeft={12}
           intent="success"
+          disabled={selectedItems.length <= 0}
         >
           Create Group
         </Button>
@@ -26,4 +31,17 @@ class NewGroupForm extends React.PureComponent {
   }
 }
 
-export default NewGroupForm
+const mapStateToProps = state => {
+  return {
+    selectedItems: state.groups.selectedItems,
+  }
+}
+
+// const mapDispatchToProps = dispatch => bindActionCreators(
+//   {
+//     groupItemSelector
+//   },
+//   dispatch
+// )
+
+export default connect(mapStateToProps, null)( NewGroupForm )
