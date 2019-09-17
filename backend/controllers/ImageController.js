af760abe7599145ccb7817bf1b9753a19f30f0d6
AKimul Akash <akimul.akash@nhst.com>
2019-09-17 14:04:53 +0600

    added exception on image controller

--- backend/controllers/ImageController.js
+++ backend/controllers/ImageController.js
@@ -1,4 +1,5 @@
 const { Terminal } = require('../utilities/terminal')
+const { lightImageDetail } = require('../utilities/lightImageDetail')
 
 exports.fetch = async (req, res) => {
   const images = await Terminal(
@@ -9,14 +10,34 @@ exports.fetch = async (req, res) => {
     .filter(image => image !== '')
     .map(image => JSON.parse(image))
   res.json(imagesArray)
+
+
+  // const rawImages = await Terminal('docker image ls -a')
+  // const images = rawImages
+  //   .split("\n")
+  //   .filter(image => image.ID !== 'IMAGE ID')
+    
+  // let results = []
+  // await Promise.all(images.map(async image => {
+  //   const singleImage = await Terminal('docker image inspect '+image.ID)
+  //   const singleImageData = JSON.parse(singleImage)[0]
+  //   results.push(lightImageDetail(image, singleImageData))
+  // }))
+  // res.json(results)
 }
 
-exports.command = async (req, res) => {
+exports.command = async (req, res, next) => {
   const imageID = req.query.image
   const command = req.query.command
   const cmd = `docker image ${command} ${imageID}`
-  const cmdData = await Terminal(cmd)
-  res.json(
-    cmdData.replace("\n", "")
-  )
+  
+  try{
+    const cmdData = await Terminal(cmd)
+    res.json(
+      cmdData
+        .replace("\n", "")
+    )
+  } catch (error){
+    next(error)
+  }
 }
