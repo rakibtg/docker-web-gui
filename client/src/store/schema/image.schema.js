5cb06b412b39c9dc2aa0d21d2e9677ddea517235
AKimul Akash <akimul.akash@nhst.com>
2019-09-17 11:03:12 +0600

    image schema added

---
+++ client/src/store/schema/image.schema.js
@@ -0,0 +1,10 @@
+export default {
+   images: [],
+   loading: false,
+   pageError: false,
+   activeIndex: 0,
+   isShowingSideSheet: false,
+   logData: {},
+   showModal: false,
+   selectedImage: {}
+ }
