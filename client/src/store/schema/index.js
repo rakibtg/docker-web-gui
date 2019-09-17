ca7c1f03c3c944f0a4aaf9b3cfa0558706f4955e
AKimul Akash <akimul.akash@nhst.com>
2019-09-17 11:03:28 +0600

    image index updated

--- client/src/store/schema/index.js
+++ client/src/store/schema/index.js
@@ -1,9 +1,11 @@
 import stats from './stats.schema'
 import groups from './groups.schema'
 import container from './container.schema'
+import image from './image.schema'
 
 export default {
   stats,
   groups,
   container,
+  image
 }
