c7fe1391e30e44d63e42b29bb30ca7bfadf16a3e
rakibtg <rakibtg@gmail.com>
2019-09-18 22:07:53 +0600

    added fetch controller

--- backend/controllers/GroupController.js
+++ backend/controllers/GroupController.js
@@ -9,3 +9,8 @@ exports.create = async (req, res) => {
   const response = await db.newGroup({name, containers})
   res.json(response)
 }
+
+exports.fetch = async (req, res) => {
+  const response = await db.getGroups()
+  res.json(response)
+}
