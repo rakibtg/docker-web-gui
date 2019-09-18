6c9edef01ee7e9229d93c04431aaeaae40d59659
AKimul Akash <akimul.akash@nhst.com>
2019-09-18 14:01:33 +0600

    modified list sidesheet component

--- client/src/components/container/lists.js
+++ client/src/components/container/lists.js
@@ -3,7 +3,7 @@ import { Pane } from 'evergreen-ui'
 
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { getContainers, toggleDeleteModal } from '../../store/actions/container.action'
+import { getContainers, toggleDeleteModal, resetLogSideSheet } from '../../store/actions/container.action'
 
 import ContainerCard from './card'
 import LogSideSheet from '../LogSideSheet'
@@ -16,14 +16,15 @@ class ContainersList extends React.PureComponent {
   }
 
   render () {
-    const { containers, showModal, selectedContainer, toggleDeleteModal} = this.props
+    const { containers, showModal, selectedContainer, toggleDeleteModal, 
+      resetLogSideSheet, isShowingSideSheet, logData} = this.props
     return <Pane 
       display="flex" 
       flexDirection="column" 
       justifyContent="center" 
       alignItems="center"
       marginTop={20}>
-      <LogSideSheet />
+      <LogSideSheet resetLogSideSheet={resetLogSideSheet} isShowingSideSheet={isShowingSideSheet} logData={logData} />
       { showModal && <Modal container={selectedContainer} toggleModal={toggleDeleteModal} />} 
         {
           containers.map((container, index) => 
@@ -44,14 +45,17 @@ const mapStateToProps = state => {
     segment: state.container.segment,
     containers: state.container.containers,
     showModal: state.container.showModal,
-    selectedContainer: state.container.selectedContainer
+    selectedContainer: state.container.selectedContainer,
+    isShowingSideSheet: state.container.isShowingSideSheet,
+    logData: state.container.logData
   }
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators(
   {
     getContainers,
-    toggleDeleteModal
+    toggleDeleteModal,
+    resetLogSideSheet
   },
   dispatch
 )
