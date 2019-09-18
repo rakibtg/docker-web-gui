d379175b0ba463445e01b17660a1c36d028ffb8d
AKimul Akash <akimul.akash@nhst.com>
2019-09-18 12:58:11 +0600

    prune action added

--- client/src/store/actions/cleanUp.action.js
+++ client/src/store/actions/cleanUp.action.js
@@ -1,5 +1,12 @@
+import { request } from '../../utilities/request'
+
 export const setCleanUpSegment = payload => ({
   type: 'SELECTED_SEGMENT',
+  payload
+})
+
+export const executePrune = payload => ({
+  type: 'EXECUTE_PRUNE',
   payload
 })
 
@@ -8,4 +15,15 @@ export const setSelectedCleanUpSegment = (value) => (dispatch, getState)=>{
   dispatch(setCleanUpSegment({
     segmentValue: value
   }))
+}
+
+
+export const pruneCommand = (type) => (dispatch, getState)=>{
+  request('get', `container/command?type=${type}`)
+    .then(res => {
+      dispatch(executePrune({
+        isShowingSideSheet: true,
+        responseData: res.Data
+      }))
+    })  
 }
