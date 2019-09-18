bb35f13af50b405155381467a4240e0b4929de1f
rakibtg <rakibtg@gmail.com>
2019-09-18 22:14:03 +0600

    store setup for groups page

--- client/src/components/GroupsList.js
+++ client/src/components/GroupsList.js
@@ -1,6 +1,17 @@
 import React from 'react'
 
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import { getGroups } from '../store/actions/groups.action'
+
 class GroupsList extends React.PureComponent {
+
+  componentDidMount () {
+    const { getGroups } = this.props
+    getGroups()
+  }
+
   render () {
     return <div>
       Groups List
@@ -8,4 +19,17 @@ class GroupsList extends React.PureComponent {
   }
 }
 
-export default GroupsList
+const mapStateToProps = state => {
+  return {
+    groups: state.groups.groups,
+  }
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators(
+  {
+    getGroups
+  },
+  dispatch
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)( GroupsList )
