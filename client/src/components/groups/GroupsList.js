5133bbc4065199203269c40a0540967ad49e0cef
rakibtg <rakibtg@gmail.com>
2019-09-19 22:35:18 +0600

    wait for both groups and containers, design fixes

--- client/src/components/groups/GroupsList.js
+++ client/src/components/groups/GroupsList.js
@@ -1,25 +1,34 @@
 import React from 'react'
-import { Pane } from 'evergreen-ui'
+import { Pane, Heading } from 'evergreen-ui'
 
 import GroupCard from './GroupCard'
 
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
+import { getContainers } from '../../store/actions/container.action'
 import { getGroups } from '../../store/actions/groups.action'
 
 class GroupsList extends React.PureComponent {
 
   componentDidMount () {
-    const { getGroups } = this.props
+    const { getGroups, getContainers } = this.props
     getGroups()
+    getContainers('all')
   }
 
   renderGroupsList () {
-    const { groups } = this.props
+    const { groups, groupListLoading, containerListLoading } = this.props
+    if( groupListLoading && containerListLoading ) {
+      return <Heading size={600}>Please wait</Heading>
+    }
+    if( groups.length <= 0 ) {
+      return <Heading size={600}>No groups found, please create a new one.</Heading>
+    }
     return groups.map((group, index) => {
       return <GroupCard 
         key={index}
+        index={index}
         group={group}
       />
     })
@@ -40,12 +49,15 @@ class GroupsList extends React.PureComponent {
 const mapStateToProps = state => {
   return {
     groups: state.groups.groups,
+    groupListLoading: state.groups.groupListLoading,
+    containerListLoading: state.container.containerListLoading,
   }
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators(
   {
-    getGroups
+    getGroups,
+    getContainers,
   },
   dispatch
 )
