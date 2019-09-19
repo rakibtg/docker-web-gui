ed9c07df3e249f21323ae98a0568d5ad61e62317
rakibtg <rakibtg@gmail.com>
2019-09-19 22:37:05 +0600

    added activeInde in container schema

--- client/src/store/schema/groups.schema.js
+++ client/src/store/schema/groups.schema.js
@@ -3,6 +3,7 @@ export default {
   selectedItems: [],
   showGroupsPage: true,
   showNewGroupForm: false,
+  activeIndex: 0,
   newGroupName: '',
   createFormLoading: false,
   groupListLoading: true,
