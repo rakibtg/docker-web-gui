3b3b2eac3524d529c90afbfee6f2c64dca7f9acc
AKimul Akash <akimul.akash@nhst.com>
2019-09-20 12:56:45 +0600

    added modal and sidesheet here

--- client/src/pages/container.page.js
+++ client/src/pages/container.page.js
@@ -4,11 +4,15 @@ import SecondaryNavBar from '../components/SecondaryNavBar'
 import ContainerLists from '../components/container/lists'
 import GroupsList from '../components/groups/GroupsList'
 
-import { connect } from 'react-redux'
-
 import {containerStatsProcess} from '../store/actions/stats.action'
 
 import {store} from '../store'
+
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { toggleDeleteModal, resetLogSideSheet } from '../store/actions/container.action'
+import LogSideSheet from '../components/LogSideSheet'
+import Modal from '../components/container/deleteModal'
 
 class ContainerPage extends React.PureComponent {
 
@@ -17,9 +21,12 @@ class ContainerPage extends React.PureComponent {
   }
 
   render () {
-    const { showGroupsPage } = this.props
+    const { showGroupsPage,showModal, selectedContainer, toggleDeleteModal, 
+      resetLogSideSheet, isShowingSideSheet, logData } = this.props
     return <>
       <SecondaryNavBar />
+      <LogSideSheet resetLogSideSheet={resetLogSideSheet} isShowingSideSheet={isShowingSideSheet} logData={logData} />
+      { showModal && <Modal container={selectedContainer} toggleModal={toggleDeleteModal} />} 
       <div className="subnavaware-view">
         {
           showGroupsPage
@@ -35,7 +42,19 @@ class ContainerPage extends React.PureComponent {
 const mapStateToProps = state => {
   return {
     showGroupsPage: state.groups.showGroupsPage,
+    showModal: state.container.showModal,
+    selectedContainer: state.container.selectedContainer,
+    isShowingSideSheet: state.container.isShowingSideSheet,
+    logData: state.container.logData
   }
 }
 
-export default connect(mapStateToProps, null)( ContainerPage )
+const mapDispatchToProps = dispatch => bindActionCreators(
+  {
+    toggleDeleteModal,
+    resetLogSideSheet
+  },
+  dispatch
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)( ContainerPage )
