721cd22802d3df5ca6cf31b447293344cc43d533
rakibtg <rakibtg@gmail.com>
2019-09-30 22:32:23 +0600

    portal based delete modal with action dispatcher

--- client/src/components/groups/GroupDeleteButton.js
+++ client/src/components/groups/GroupDeleteButton.js
@@ -1,18 +1,68 @@
 import React from 'react'
-import { Button } from 'evergreen-ui'
+import { Button, Dialog, Heading, Badge } from 'evergreen-ui'
+import ReactDOM from 'react-dom'
+
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { deleteGroup } from '../../store/actions/groups.action'
 
 class GroupDeleteButton extends React.PureComponent {
+
+  state = {
+    deleteModal: false,
+  }
+
+  renderDeleteModal () {
+    const { groupName, groupId, deleteGroup } = this.props
+    if(this.state.deleteModal) {
+      return ReactDOM.createPortal(
+        <Dialog
+          isShown={this.state.deleteModal}
+          title="Confirmation"
+          hasHeader={false}
+          intent="danger"
+          onConfirm={() => {
+            deleteGroup(groupId)
+            this.setState({
+              deleteModal: false,
+            })
+          }}
+          onCloseComplete={() => this.setState({ deleteModal: false })}
+          confirmLabel="Confirm"
+        >
+          <Heading size={500} marginTop="default" marginBottom="default">
+            Are you sure you want to delete <Badge color="neutral">{groupName}</Badge> container group?
+          </Heading>
+        </Dialog>,
+        document.body
+      )
+    } else {
+      return null
+    }
+  }
+
   render () {
-    return <Button marginRight={5} 
-      height={22} 
-      iconBefore="trash" 
-      // disabled={container.State.Running}
-      onClick={() => {
-        // toggleDeleteModal(container)
-      }}>
-      Delete Group
-    </Button>
+    return <>
+      <Button marginRight={5} 
+        height={22} 
+        iconBefore="trash"
+        onClick={() => {
+          this.setState({
+            deleteModal: !this.state.deleteModal
+          })
+        }}>
+        Delete Group
+      </Button>
+      {this.renderDeleteModal()}
+    </>
   }
 }
 
-export default GroupDeleteButton
+const mapDispatchToProps = dispatch => bindActionCreators(
+  {
+    deleteGroup,
+  },
+  dispatch
+)
+
+export default connect(null, mapDispatchToProps)( GroupDeleteButton )
