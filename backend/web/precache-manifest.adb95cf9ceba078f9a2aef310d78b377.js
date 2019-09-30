b97da73091013d89198c7f085ec2b0aef27b14c8
rakibtg <rakibtg@gmail.com>
2019-09-30 23:58:09 +0600

    added web build

---
+++ backend/web/precache-manifest.adb95cf9ceba078f9a2aef310d78b377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f5dd4b3b8d5e2a5e8a098dcc010693c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6032ca2c0c37d3a1e3fd",
+    "url": "/static/css/main.ab6a2c25.chunk.css"
+  },
+  {
+    "revision": "666d35ac2d9669737dde",
+    "url": "/static/js/2.f3d6def3.chunk.js"
+  },
+  {
+    "revision": "6032ca2c0c37d3a1e3fd",
+    "url": "/static/js/main.5147f23d.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
