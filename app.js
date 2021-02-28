7a6e0a9b0196200a785d498694c00757ab7ea4e7
rakibtg <rakibtg@gmail.com>
2021-02-28 17:11:33 +0600

    Consume installModules and serve from safeTerminal

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const fs = require('fs')
-const path = require('path')
-const { Terminal } = require('./backend/utilities/terminal')
-const port = 3230
+const fs = require("fs");
+const path = require("path");
+const { safeTerminal } = require("./backend/utilities/terminal");
+const port = 3230;
 
-async function app () {
-  console.clear()
-  
-  const BACKEND = path.resolve(__dirname + '/backend/')
-  const NODE_MODULES = BACKEND + '/node_modules'
+async function app() {
+  console.clear();
 
-  if(!fs.existsSync(NODE_MODULES)) {
-    console.log('🚀  Please wait while we install all the dependencies for you...\n')
-    opt = await Terminal(`cd ${BACKEND} && npm install`)
-    console.log('🎉  All dependencies added successfully!')
+  const BACKEND = path.resolve(__dirname + "/backend/");
+  const NODE_MODULES = BACKEND + "/node_modules";
+
+  if (!fs.existsSync(NODE_MODULES)) {
+    console.log(
+      "🚀  Please wait while we install all the dependencies for you...\n"
+    );
+    await safeTerminal.installModules(BACKEND);
+    console.log("🎉  All dependencies added successfully!");
   }
 
   setTimeout(() => {
-    console.log(`✨  Visit http://localhost:${port} to use Docker Web GUI`)
-  }, 1500)
-  await Terminal(`cd ${BACKEND} && node index.js`)
-
+    console.log(`✨  Visit http://localhost:${port} to use Docker Web GUI`);
+  }, 1500);
+  await safeTerminal.serve(BACKEND);
 }
 
-app()
+app();
