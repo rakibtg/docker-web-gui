16779d0950a706b2fc905cc9b9514d3511142f4f
rakibtg <rakibtg@gmail.com>
2021-02-28 17:16:09 +0600

    Consume formattedImages and singleImage from safeTerminal

--- backend/controllers/ImageController.js
+++ backend/controllers/ImageController.js
@@ -1,32 +1,22 @@
-const { Terminal } = require('../utilities/terminal')
-const { lightImageDetail } = require('../utilities/lightImageDetail')
+const { safeTerminal } = require("../utilities/terminal");
+const { lightImageDetail } = require("../utilities/lightImageDetail");
 
 exports.fetch = async (req, res) => {
-  const images = await Terminal(
-    `docker images --format '{"ID": "{{.ID}}", "Tag": "{{.Tag}}", "CreatedSince": "{{.CreatedSince}}", "Size": "{{.Size}}", "VirtualSize": "{{.VirtualSize}}", "Repository": "{{.Repository}}"}'`
-  )
+  const images = await safeTerminal.formattedImages();
   const imagesArray = images
     .split("\n")
-    .filter(image => image !== '')
-    .map(image => JSON.parse(image))
-  res.json(imagesArray)
-}
+    .filter((image) => image !== "")
+    .map((image) => JSON.parse(image));
+  res.json(imagesArray);
+};
 
 exports.command = async (req, res, next) => {
-  const imageID = req.query.image
-  const command = req.query.command
-  let cmd = `docker image ${command} ${imageID}`
-  if(command == 'run') {
-    cmd = `docker ${command} ${imageID}`
+  const imageID = req.query.image;
+  const command = req.query.command;
+  try {
+    const cmdData = await safeTerminal.singleImage(command, imageID);
+    res.json(cmdData.replace("\n", ""));
+  } catch (error) {
+    next(error);
   }
-  
-  try{
-    const cmdData = await Terminal(cmd)
-    res.json(
-      cmdData
-        .replace("\n", "")
-    )
-  } catch (error){
-    next(error)
-  }
-}
+};
