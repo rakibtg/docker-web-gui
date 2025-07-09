25876f46f524f3a4e12d34942a51cfb06d0ec5be
rakibtg <rakibtg@gmail.com>
2025-07-09 13:43:49 +0200

    feat: add main entry point for the application with theme support

---
+++ packages/client/src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeProvider } from "./contexts/ThemeContext";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
