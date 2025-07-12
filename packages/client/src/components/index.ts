4c9db2ee8a880cadce605e885faaee54cf9e9a26
rakibtg <rakibtg@gmail.com>
2025-07-12 02:20:08 +0200

    feat: remove Tooltip export to streamline component index

--- packages/client/src/components/index.ts
+++ packages/client/src/components/index.ts
@@ -10,4 +10,3 @@ export { ToggleSwitch } from "./ToggleSwitch";
 export { UptimeDisplay } from "./UptimeDisplay";
 export { Terminal } from "./Terminal";
 export { TerminalManager } from "./TerminalManager";
-export { Tooltip } from "./Tooltip";
