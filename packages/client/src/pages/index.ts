89c51ec1016747326dff82fdb66835ea4851096f
rakibtg <rakibtg@gmail.com>
2025-07-14 01:56:04 +0200

    feat: create index file to export main application pages

---
+++ packages/client/src/pages/index.ts
@@ -0,0 +1,7 @@
+export { Dashboard } from "./Dashboard";
+export { ContainersPage } from "./ContainersPage";
+export { Images } from "./Images";
+export { Networks } from "./Networks";
+export { Volumes } from "./Volumes";
+export { Settings } from "./Settings";
+export { About } from "./About";
