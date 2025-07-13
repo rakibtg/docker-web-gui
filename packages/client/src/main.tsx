b9aeeffee814680ad62afd1d080503e7f0a76c02
rakibtg <rakibtg@gmail.com>
2025-07-13 23:30:18 +0200

    feat: wrap App component with RouterProvider for improved routing context

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { RouterProvider } from "./contexts/RouterContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider>
+        <App />
+      </RouterProvider>
     </ThemeProvider>
   </StrictMode>
 );
