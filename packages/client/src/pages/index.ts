1c794b6b6384bf44597423d85457f94d37451cca
rakibtg <rakibtg@gmail.com>
2025-07-16 13:40:15 +0200

    feat: export VolumeDetails component in index file for improved accessibility

--- packages/client/src/pages/index.ts
+++ packages/client/src/pages/index.ts
@@ -3,5 +3,6 @@ export { ContainersPage } from "./ContainersPage";
 export { Images } from "./Images";
 export { Networks } from "./Networks";
 export { Volumes } from "./Volumes";
+export { VolumeDetails } from "./VolumeDetails";
 export { Settings } from "./Settings";
 export { About } from "./About";
