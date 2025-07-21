fdd0f1b30d1554eaa39a65934c3a8a6b9a24fd25
rakibtg <rakibtg@gmail.com>
2025-07-21 15:03:49 +0200

    feat: add index file for exporting context and types

---
+++ packages/client/src/contexts/index.ts
@@ -0,0 +1,8 @@
+export { AppContext } from "./context";
+export { AppProvider } from "./AppContext";
+export type {
+  AppContextType,
+  TerminalSession,
+  ImageHistoryState,
+  ImageHistoryLayer,
+} from "./types";
