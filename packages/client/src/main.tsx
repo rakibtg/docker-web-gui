a320dbd1079c3f508e4dab68a4373037068cc933
rakibtg <rakibtg@gmail.com>
2025-07-22 19:53:01 +0200

    fix: replace RouterProvider with BrowserRouter for improved routing

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import { RouterProvider } from "./contexts/RouterContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <RouterProvider>
+      <BrowserRouter>
         <App />
-      </RouterProvider>
+      </BrowserRouter>
     </ThemeProvider>
   </StrictMode>
 );
