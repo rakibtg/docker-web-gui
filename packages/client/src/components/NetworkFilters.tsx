6787d83874e6eab3f1e4b44f551ab2a7881acb8e
rakibtg <rakibtg@gmail.com>
2025-07-30 00:14:48 +0200

    refactor: simplify NetworkFilters by removing unused props and integrating SearchInput

--- packages/client/src/components/NetworkFilters.tsx
+++ packages/client/src/components/NetworkFilters.tsx
@@ -1,22 +1,19 @@
 import { memo } from "react";
-import { FaSearch, FaFilter } from "react-icons/fa";
+import SearchInput from "./SearchInput";
+import { FaFilter } from "react-icons/fa";
 
 interface NetworkFiltersProps {
+  searchTerm: string;
+  selectedDriver: string;
   onSearch: (searchTerm: string) => void;
   onDriverFilter: (driver: string) => void;
-  totalNetworks: number;
-  filteredNetworks: number;
-  selectedDriver: string;
-  searchTerm: string;
 }
 
 const NetworkFilters = memo(function NetworkFilters({
   onSearch,
-  onDriverFilter,
-  totalNetworks,
-  filteredNetworks,
-  selectedDriver,
   searchTerm,
+  onDriverFilter,
+  selectedDriver,
 }: NetworkFiltersProps) {
   const driverOptions = [
     { value: "", label: "All Drivers" },
@@ -32,26 +29,20 @@ const NetworkFilters = memo(function NetworkFilters({
     <>
       <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
         <div className="flex flex-col sm:flex-row gap-4 flex-1">
-          {/* Search */}
-          <div className="relative flex-1 max-w-md">
-            <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
-            <input
-              type="text"
-              placeholder="Search networks..."
-              value={searchTerm}
-              onChange={(e) => onSearch(e.target.value)}
-              className="w-full pl-10 pr-4 py-2 border border-gray-300 dark:border-gray-600 rounded-md bg-white dark:bg-gray-700 text-gray-100 placeholder-gray-500 dark:placeholder-gray-400 focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-            />
-          </div>
+          <SearchInput
+            value={searchTerm}
+            onChange={onSearch}
+            placeholder="Search networks by name or ID..."
+          />
 
           {/* Driver Filter */}
           <div className="relative">
-            <FaFilter className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
+            <FaFilter className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 w-3 h-3" />
             <select
               value={selectedDriver}
               onChange={(e) => onDriverFilter(e.target.value)}
               title="Filter by driver"
-              className="pl-10 pr-8 py-2 border border-gray-300 dark:border-gray-600 rounded-md bg-white dark:bg-gray-700 text-gray-100 focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              className="pl-6 pr-8 py-[7.5px] text-sm bg-gray-800 rounded-lg border border-gray-600 text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-800"
             >
               {driverOptions.map((option) => (
                 <option key={option.value} value={option.value}>
@@ -61,11 +52,6 @@ const NetworkFilters = memo(function NetworkFilters({
             </select>
           </div>
         </div>
-      </div>
-
-      {/* Results Count */}
-      <div className="text-sm text-gray-400">
-        Showing {filteredNetworks} of {totalNetworks} networks
       </div>
     </>
   );
